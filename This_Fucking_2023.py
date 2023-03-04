47d3112d049d1112e584baff069da6c57af0daad
CallCenterElizar <ivanovilya14122000@yandex.ru>
2023-03-04 13:59:16 +0700

    ДОБАВИЛ ССЫЛОЧКУ

--- This_Fucking_2023.py
+++ This_Fucking_2023.py
@@ -1,3 +1,4 @@
 # It's me first project. I am don't read and speak an England language. His date 14.02.2023
 # and i say yourself. I viuchu Python. Ilya, viev an this date and this text. Now you luchshe
 # Spasibo
+# https://github.com/Ivan-Zaitsev/SMSBomber300.git
